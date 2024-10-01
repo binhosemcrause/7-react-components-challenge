e6227c1ad0612c48d1331c0ea9a95a6b43cea331
binhosemcrause <binhosemcrause@gmail.com>
2024-10-01 19:01:27 +1000

    upload project

---
+++ src/index.js
@@ -0,0 +1,15 @@
+//Create a React app from scratch.
+//Show a single h1 that says "Good morning" if between midnight and 12PM.
+//or "Good Afternoon" if between 12PM and 6PM.
+//or "Good evening" if between 6PM and midnight.
+//Apply the "heading" style in your CSS
+//Dynamically change the color of the h1 using inline css styles.
+//Morning = red, Afternoon = green, Night = blue.
+//Use react components to do it
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm start
+// to launch your react project in your browser
+
